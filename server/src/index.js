ec984c5047cdaffce8cb977114694f2d34f948e5
Sarthak <sarthaksaxena2604@gmail.com>
2026-05-19 13:13:24 +0530

    fix cors url

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hirethropic.vercel.app'],
+  origin: ['http://localhost:5173', 'https://hire-thropic.vercel.app'],
   credentials: true,
 }));
 
